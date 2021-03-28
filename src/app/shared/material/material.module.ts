66a9bc1486cfaf7f39b77dcfdd93822b1c35abb9
DominikMaroszczyk <domi.m1222@gmail.com>
2021-03-28 19:08:26 +0200

    Change fetch planets method and sorting
    Sorting whole list of planets and fetching all at once

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-const MaterialComponents = [MatButtonModule, MatCardModule, MatIconModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
--- src/app/shared/services/planet.service.ts
+++ src/app/shared/services/planet.service.ts
@@ -1,13 +1,77 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 import { PlanetModel } from '../models/Planet.model';
+import { SwapiService } from './swapi.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlanetService {
   private _currentPlanet: PlanetModel;
+  private _planetsOnPage: number = 10;
 
-  constructor() {}
+  private _numOfPages = new BehaviorSubject<number>(null);
+  private _planets = new BehaviorSubject<PlanetModel[]>(null);
+
+  constructor(private _swapiService: SwapiService) {
+    this.getPlanetsFromSwapi();
+  }
+
+  //
+  private getPlanetsFromSwapi() {
+    let subject = new Subject();
+    let tempArray = new Array<PlanetModel>();
+
+    subject.subscribe((pageNum: number) => {
+      this._swapiService
+        .getPlanets(pageNum)
+        .pipe(
+          tap((data) => {
+            if (data.next != null) {
+              pageNum++;
+              subject.next(pageNum);
+            } else {
+              this._numOfPages.next(this.countPagesAmount(data.count));
+              subject.complete();
+            }
+          })
+        )
+        .subscribe((res) => {
+          tempArray.push(...res.results);
+        });
+    });
+
+    subject.next(1);
+
+    subject.toPromise().then(() => {
+      this._planets.next(tempArray.sort(PlanetModel.compareFn));
+    });
+  }
+
+  //
+  private countPagesAmount(elements: number): number {
+    return (
+      Math.floor(elements / this._planetsOnPage) +
+      (elements % this._planetsOnPage > 0 ? 1 : 0)
+    );
+  }
+
+  //
+  getPlanets(): BehaviorSubject<PlanetModel[]> {
+    return this._planets;
+  }
+
+  //
+  getNumOfPages(): BehaviorSubject<number> {
+    return this._numOfPages;
+  }
+
+  //
+  getPlanetsOnPage(): number {
+    return this._planetsOnPage;
+  }
 
   //
   setCurrentPlanet(planet: PlanetModel) {
